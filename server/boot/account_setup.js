7fc89ca2aced1686dce3d8f0943b8cb35021ba41
vip1all <vip.all@hotmail.com>
2018-07-10 15:07:24 +0200

    Created boot script that creates accounts based on accounts-config file :)

--- server/boot/account_setup.js
+++ server/boot/account_setup.js
@@ -4,5 +4,25 @@ const loadConfig = require('../util/load-config');
 
 module.exports = function(app) {
   const Member = app.models.Member;
-  const config = loadConfig('./accounts-config');
+  const config = loadConfig('accounts-config');
+
+  if (!Array.isArray(config)) config = [];
+
+  const accPromises = config.map((account) => {
+    const filter = {
+      where: {
+        or: [{ username: account.username }, { email: account.email }],
+      },
+    };
+
+    return Member.find(filter).then(function(result) {
+      if (result.length === 0) {
+        return Member.create(account);
+      } else {
+        return Promise.resolve();
+      }
+    });
+  });
+
+  return Promise.all(accPromises);
 };
